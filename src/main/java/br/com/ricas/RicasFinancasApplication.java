eb1a37752d6e2c11b376dcc443ac1b09c13bf369
Ricardo Mello <ricardo.mello@luizalabs.com>
2020-10-28 22:09:20 -0300

    first commit

---
+++ src/main/java/br/com/ricas/RicasFinancasApplication.java
@@ -0,0 +1,13 @@
+package br.com.ricas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RicasFinancasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RicasFinancasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ricas/RicasFinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ricas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RicasFinancasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
