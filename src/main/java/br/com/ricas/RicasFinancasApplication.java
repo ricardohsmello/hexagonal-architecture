76bf9148e93f4fbd2e5de728290588a3412d80f2
Ricardo Mello <ricardo.mello@luizalabs.com>
2020-10-31 00:50:59 -0300

    ajustes

--- src/main/java/br/com/ricas/RicasFinancasApplication.java
+++ src/main/java/br/com/ricas/RicasFinancasApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RicasFinancasApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RicasFinancasApplication.class, args);
 	}
-
-
 }
