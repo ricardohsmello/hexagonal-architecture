0a53d4fc6dcbfb46795d6680f5e46b1cb95980d0
Ricardo Mello <ricardo.mello@luizalabs.com>
2020-11-20 18:26:27 -0300

    Includint builder

--- infrastructure/adapter-persistence-jpa/src/main/java/br/com/ricas/entity/FinanceEntity.java
+++ infrastructure/adapter-persistence-jpa/src/main/java/br/com/ricas/entity/FinanceEntity.java
@@ -1,11 +1,13 @@
 package br.com.ricas.entity;
 
+import lombok.Builder;
 import lombok.Data;
 
 import javax.persistence.*;
 
 @Entity
 @Data
+@Builder(toBuilder = true)
 @Table(name = "FINANCE")
 public class FinanceEntity {
 
